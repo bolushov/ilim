c09aea8fc2fb9fd6c96264f13aabbd0d70c19c72
bolushov <93927365+bolushov@users.noreply.github.com>
2021-11-08 20:35:53 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 
 <h2>HTML Image</h2>
-<img src="ilim.jp" alt="Flowers in Chania" width="460" height="345">
+<img src="ilim.jap" alt="ilims image" width="460" height="345">
 
 </body>
 </html>
